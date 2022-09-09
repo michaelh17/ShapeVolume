2e570b96ef3716487719237966b0771cf33bbb48
unknown <michaelxu17@gmail.com>
2022-09-09 21:41:19 +0700

    add SegitigaPage

---
+++ app/src/main/java/com/mobile/example/aplikasipenghitungbentuk/SegitigaPage.java
@@ -0,0 +1,14 @@
+package com.mobile.example.aplikasipenghitungbentuk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegitigaPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segitiga_page);
+    }
+}
